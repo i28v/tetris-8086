cdb6ca4427d239137847479cb8c43fe5720ae473
i28v <legomail90520@gmail.com>
2024-03-02 14:37:55 -0800

    Add files via upload

--- tetris.asm
+++ tetris.asm
@@ -1,66 +1,73 @@
 bits 16
-org 0x100
+org 0x0100
 
-jmp start
+jmp start_game
 
 align 4
 
-scoremsg db "Score:     0"
-linesmsg db "Lines:     0"
+score_msg db "Score:     0"
+lines_msg db "Lines:     0"
 
-TN db 0x01, 0x11, 0x21, 0x10, 0x32, 0x00
+TN db 0x01, 0x11, 0x21, 0x10, 0x32, 0x50
 dw 0x0000
-TE db 0x00, 0x01, 0x02, 0x11, 0x23, 0x00
+TE db 0x00, 0x01, 0x02, 0x11, 0x23, 0x50
 dw 0x0000
-TS db 0x00, 0x10, 0x20, 0x11, 0x32, 0x00
+TS db 0x00, 0x10, 0x20, 0x11, 0x32, 0x50
 dw 0x0000
-TW db 0x10, 0x11, 0x12, 0x01, 0x23, 0x01
+TW db 0x10, 0x11, 0x12, 0x01, 0x23, 0x51
 dw TN
-IV db 0x00, 0x01, 0x02, 0x03, 0x14, 0x00
+IV db 0x00, 0x01, 0x02, 0x03, 0x14, 0x30
 dw 0x0000
-IH db 0x03, 0x13, 0x23, 0x33, 0x44, 0x01
+IH db 0x03, 0x13, 0x23, 0x33, 0x44, 0x31
 dw IV
-SRV db 0x00, 0x01, 0x11, 0x12, 0x23, 0x00
+SRV db 0x00, 0x01, 0x11, 0x12, 0x23, 0x20
 dw 0x0000
-SRH db 0x01, 0x11, 0x10, 0x20, 0x32, 0x01
+SRH db 0x01, 0x11, 0x10, 0x20, 0x32, 0x21
 dw SRV
-SLV db 0x10, 0x11, 0x01, 0x02, 0x23, 0x00
+SLV db 0x10, 0x11, 0x01, 0x02, 0x23, 0x40
 dw 0x0000
-SLH db 0x00, 0x10, 0x11, 0x21, 0x32, 0x01
+SLH db 0x00, 0x10, 0x11, 0x21, 0x32, 0x41
 dw SLV
-LRN db 0x00, 0x01, 0x02, 0x12, 0x23, 0x00
+LRN db 0x00, 0x01, 0x02, 0x12, 0x23, 0x60
 dw 0x0000
-LRE db 0x00, 0x10, 0x20, 0x01, 0x32, 0x00
+LRE db 0x00, 0x10, 0x20, 0x01, 0x32, 0x60
 dw 0x0000
-LRS db 0x00, 0x10, 0x11, 0x12, 0x23, 0x00
+LRS db 0x00, 0x10, 0x11, 0x12, 0x23, 0x60
 dw 0x0000
-LRW db 0x01, 0x11, 0x21, 0x20, 0x32, 0x01
+LRW db 0x01, 0x11, 0x21, 0x20, 0x32, 0x61
 dw LRN
-LLN db 0x10, 0x11, 0x02, 0x12, 0x23, 0x00
+LLN db 0x10, 0x11, 0x02, 0x12, 0x23, 0x90
 dw 0x0000
-LLE db 0x00, 0x01, 0x11, 0x21, 0x32, 0x00
+LLE db 0x00, 0x01, 0x11, 0x21, 0x32, 0x90
 dw 0x0000
-LLS db 0x00, 0x10, 0x01, 0x02, 0x23, 0x00
+LLS db 0x00, 0x10, 0x01, 0x02, 0x23, 0x90
 dw 0x0000
-LLW db 0x00, 0x10, 0x20, 0x21, 0x32, 0x01
+LLW db 0x00, 0x10, 0x20, 0x21, 0x32, 0x91
 dw LLN
-O db 0x00, 0x10, 0x01, 0x11, 0x22, 0xFF
+O db 0x00, 0x10, 0x01, 0x11, 0x22, 0xE2
 dw 0x0000
 
-sp1 dw TE
-sp2 dw IV
-sp3 dw SRV
-sp4 dw SLV
-sp5 dw LRS
-sp6 dw LLS
-sp7 dw O
+starting_pieces:
+dw TE
+dw IV
+dw SRV
+dw SLV
+dw LRS
+dw LLS
+dw O
+
+score_multipliers:
+dw 1
+dw 40
+dw 100
+dw 300
+dw 1200
 
 score resw 1
 lines resw 1
 piece_x resb 1
 piece_y resb 1
 
-collision resb 1
 drop resb 1
 board_update resb 1
 down_delay resb 1
@@ -73,12 +80,14 @@ random_seed resw 1
 
 running resb 1
 
-board resb 200 
+board resb 400 
 
 screen_buffer equ 0xB800
 
-score_offset equ 3360
-lines_offset equ 3520
+
+board_offset equ 64
+score_offset equ 3422
+lines_offset equ 3582
 
 rotate_key equ 0x48
 down_key equ 0x50
@@ -87,31 +96,30 @@ right_key equ 0x4D
 drop_key equ 0x3920
 exit_key equ 0x1B
 
-moving_piece equ 0x23
-still_piece equ 0x63
-empty_space equ 0x2E
-piece_mask equ 0x3F
+moving_piece equ 0xDB
+still_piece equ 0xDF
+empty_space equ 0x20
+piece_mask equ 0xFB
+
+border_char equ 0x01BA
 
 starting_x equ 4
 starting_y equ 0
 
-score_increment_value equ 250
-
 starting_speed equ 20
 speed_change_threshold equ 3
-minimum_speed equ 2
+minimum_speed equ 4
 speed_decrement_value equ 2
 delay_in_microseconds equ 50000
 
 align 4
 
-start:
+start_game:
     cld
     xor ax, ax
     mov word [score], ax
     mov word [lines], ax
     mov word [previous_piece_ptr], ax
-    mov byte [collision], al 
     mov byte [drop], al
     mov byte [down_delay], al
     mov byte [down_threshold], starting_speed
@@ -127,7 +135,7 @@ start:
     dec dx
 .start_piece_is_zero:
     shl dx, 1
-    lea bx, [sp1]
+    mov bx, starting_pieces
     add bx, dx
     mov ax, word [bx]
     mov word [current_piece_ptr], ax
@@ -141,10 +149,33 @@ start:
     xor di, di
     mov cx, 2000
     rep stosw 
+    mov ax, border_char
+    mov di, 62
+    mov cx, 20 
+.draw_borders:
+    stosw
+    add di, 20
+    stosw
+    add di, 136
+    loop .draw_borders
+    mov si, score_msg
+    mov di, score_offset
+    mov cx, 6
+.draw_score_text:
+    movsb
+    inc di
+    loop .draw_score_text
+    mov si, lines_msg
+    mov di, lines_offset
+    mov cx, 6
+.draw_lines_text:
+    movsb
+    inc di
+    loop .draw_lines_text
     pop es
-    mov ax, 0x2E2E
-    lea di, [board]
-    mov cx, 100
+    mov ax, 0x0720
+    mov di, board
+    mov cx, 200
     rep stosw
 .main_loop:
     cmp byte [board_update], 1
@@ -158,31 +189,30 @@ start:
     push es
     mov ax, screen_buffer
     mov es, ax
-    lea si, [board]
-    xor di, di 
+    mov si, board
+    mov di, board_offset
     mov cx, 20
 .print_board_y: 
     push cx
     mov cx, 10
 .print_board_x:
-    lodsb
+    lodsw
     and al, piece_mask
-    stosb
-    inc di
+    stosw
     loop .print_board_x
     add di, 140
     pop cx
     loop .print_board_y
-    lea si, [scoremsg]
-    mov di, score_offset
-    mov cx, 12
+    mov si, score_msg + 6
+    mov di, score_offset + 12
+    mov cx, 6
 .print_score:
     movsb
     inc di
     loop .print_score 
-    lea si, [linesmsg]
-    mov di, lines_offset
-    mov cx, 12
+    mov si, lines_msg + 6
+    mov di, lines_offset + 12
+    mov cx, 6
 .print_lines:
     movsb
     inc di
@@ -200,6 +230,7 @@ start:
     mov si, word [current_piece_ptr]
     mov word [previous_piece_ptr], si
     mov al, byte [si + 5]
+    and al, 0x0F
     test al, al
     jnz .notzero
     add si, 8
@@ -219,18 +250,17 @@ start:
     mov dl, al
     add ax, bx
     cmp ah, 10
-    jle .check_y
+    jbe .check_y
     mov dh, dl
     dec dh
 .check_y:
     cmp al, 20
-    jg .end_input
+    ja .end_input
     sub bh, dh
     xor dl, dl 
-    mov byte [collision], dl
     call put_piece
-    cmp byte [collision], 1
-    je .end_input
+    test al, al
+    jnz .end_input
     add bh, dh
     mov dl, empty_space
     push si
@@ -259,14 +289,13 @@ start:
     mov bl, byte [piece_y]
     add al, bl
     cmp al, 20
-    jge .set_piece
+    jae .set_piece
     mov bh, byte [piece_x]
     inc bl
     xor dl, dl
-    mov byte [collision], dl
     call put_piece
-    cmp byte [collision], 1
-    je .set_piece
+    test al, al
+    jnz .set_piece
     mov byte [piece_y], bl
     cmp byte [drop], 1
     je .down_loop
@@ -276,13 +305,10 @@ start:
     mov bh, byte [piece_x]
     mov bl, byte [piece_y]
     cmp bl, 3 
-    jg .not_at_top
+    ja .not_at_top
     mov byte [running], 0
     jmp .end_input
 .not_at_top:
-    mov dl, empty_space
-    mov si, word [current_piece_ptr]
-    call put_piece
     mov dl, still_piece
     call put_piece
     mov ax, word [random_seed]
@@ -301,54 +327,56 @@ start:
     dec ax
 .is_zero:
     shl ax, 1
-    lea bx, [sp1]
+    mov bx, starting_pieces
     add bx, ax
     mov ax, word [bx]
     mov word [current_piece_ptr], ax 
     mov byte [piece_x], starting_x
     mov byte [piece_y], starting_y
-    inc word [score]
 .check_lines:
-    lea bx, [board + 20]
+    xor si, si
+    mov bx, board + 40
     mov cx, 18 
 .line_check_y:
-    xor ax, ax
+    xor al, al
     push cx
     mov cx, 10
 .line_check_x:
     cmp byte [bx], still_piece
     je .line_check_x_done
-    mov ax, 1 
+    mov al, 1 
 .line_check_x_done:
-    inc bx
+    add bx, 2
     loop .line_check_x
-    test ax, ax
+    test al, al
     jnz .no_line
-    add word [score], score_increment_value
+    add si, 2
     mov ax, word [lines]
     inc word [lines]
     and ax, speed_change_threshold
     jnz .no_speed_increase
     cmp byte [down_threshold], minimum_speed 
-    jle .no_speed_increase
+    jbe .no_speed_increase
     sub byte [down_threshold], speed_decrement_value
 .no_speed_increase:
     push bx
 .remove_line:
-    dec bx
-    cmp bx, board + 20
-    jl .remove_line_done
-    mov al, byte [bx - 10]
-    mov byte [bx], al
+    sub bx, 2
+    cmp bx, board + 40
+    jb .remove_line_done
+    mov ax, word [bx - 20]
+    mov word [bx], ax
     jmp .remove_line
 .remove_line_done:
     pop bx
 .no_line:
     pop cx
     loop .line_check_y
-    std
+    mov ax, word [score_multipliers + si]
+    add word [score], ax
     mov dx, word [score]
-    lea di, [scoremsg + 11]
+    mov di, score_msg + 11
+    std
 .write_score:
     call div10
     or al, 0x30
@@ -356,7 +384,7 @@ start:
     test dx, dx
     jnz .write_score
     mov dx, word [lines]
-    lea di, [linesmsg + 11]
+    mov di, lines_msg + 11
 .write_lines:
     call div10
     or al, 0x30
@@ -374,11 +402,10 @@ start:
     dec bh
     mov bl, byte [piece_y]
     xor dl, dl
-    mov byte [collision], dl
     mov si, word [current_piece_ptr]
     call put_piece
-    cmp byte [collision], 1
-    je .end_input
+    test al, al
+    jnz .end_input
     mov byte [piece_x], bh
     inc bh
     jmp .end_horizontal_move
@@ -395,10 +422,9 @@ start:
     inc bh
     mov bl, byte [piece_y]
     xor dl, dl
-    mov byte [collision], dl
     call put_piece
-    cmp byte [collision], 1
-    je .end_input
+    test al, al
+    jnz .end_input
     mov byte [piece_x], bh
     dec bh
 .end_horizontal_move:
@@ -420,7 +446,7 @@ start:
 .end_input:
     mov al, byte [down_threshold]
     cmp byte [down_delay], al
-    jge .auto_down
+    jae .auto_down
     inc byte [down_delay]
     xor cx, cx
     mov dx, delay_in_microseconds
@@ -428,7 +454,7 @@ start:
     int 0x15
     cmp byte [running], 1
     je .main_loop
-end:
+end_game:
     mov ah, 1
     mov cx, 0x0607
     int 0x10
@@ -470,11 +496,16 @@ div10:
 align 4
 
 put_piece:
-    push ax
     push cx
+    push dx
     push si
     push di
-    mov cx, 4 
+    mov cx, 4
+    mov dh, 7
+    cmp dl, empty_space
+    jbe .get_position
+    mov dh, byte [si + 5]
+    shr dh, 4
 .get_position:
     lodsb
     mov ah, al
@@ -487,19 +518,21 @@ put_piece:
     add al, ah
     add di, ax
     and di, 0x00FF
+    shl di, 1
     test dl, dl
     jnz .place
+    xor al, al
     cmp byte [board + di], still_piece
     jne .continue
-    mov byte [collision], 1
+    mov al, 1
     jmp .end
 .place:
-    mov byte [board + di], dl
+    mov word [board + di], dx
 .continue:
     loop .get_position
 .end:
     pop di
     pop si
+    pop dx
     pop cx
-    pop ax
     ret
